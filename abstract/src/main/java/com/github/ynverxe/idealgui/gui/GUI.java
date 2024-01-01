14319e7342e6fe572a6868e5becddb3a83b1d9bb
Ynverxe <nahubar65@gmail.com>
2024-01-01 20:13:33 -0300

    docs: add some docs

--- abstract/src/main/java/com/github/ynverxe/idealgui/gui/GUI.java
+++ abstract/src/main/java/com/github/ynverxe/idealgui/gui/GUI.java
@@ -19,38 +19,95 @@ import java.util.function.BiPredicate;
 @SuppressWarnings("rawtypes")
 public interface GUI<Viewer, Item, Click, G extends GUI, Handle> extends Viewable<Viewer>, PropertyHolder<G> {
 
+  /**
+   * @return The title that is actually being displayed.
+   */
   @NotNull Component showingTitle();
 
+  /**
+   * Sets the title and send it to all viewers.
+   * @param component The title component
+   * @return this gui.
+   */
   @Contract("_ -> this")
   @NotNull G title(@NotNull Component component);
 
+  /**
+   * @return The open handler of this GUI.
+   */
   @NotNull BiPredicate<G, Viewer> openHandler();
 
+  /**
+   * @return The close handler of this GUI.
+   */
   @NotNull BiConsumer<G, Viewer> closeHandler();
 
+  /**
+   * @return The click handler of this GUI.
+   */
   @NotNull BiPredicate<Item, Click> clickHandler();
 
+  /**
+   * @return The GUIDesignType of this GUI. This object defines the capacity
+   * and the slot format of the GUI.
+   */
   @NotNull GUIDesignType type();
 
+  /**
+   * @return The platform handle of this GUI.
+   */
   @NotNull Handle handle();
 
+  /**
+   * @return The {@link Pagination} of this GUI.
+   */
   @NotNull Pagination pagination();
 
+  /**
+   * @return A view of the rendered items.
+   */
   @NotNull ItemHolder<Item, Click> renderedItemsView();
 
+  /**
+   * @return An {@link ItemContainer} holding the static items of this gui.
+   */
   @NotNull ItemContainer<Item, Click, G> staticItems();
 
+  /**
+   * @return An {@link ItemContainer.Accumulative} holding the normal items of this gui.
+   */
   @NotNull ItemContainer.Accumulative<Item, Click, G> items();
 
+  /**
+   * Relocate the normal items in order of the static items.
+   * @see com.github.ynverxe.idealgui.gui.render.ItemRelocator
+   *
+   * @param relocate the option
+   * @return this gui.
+   */
   @Contract("_ -> this")
   @NotNull G relocateItems(boolean relocate);
 
+  /**
+   * @return A {@link ComposedGUIRenderer} to customize item rendering.
+   */
   @NotNull ComposedGUIRenderer<Item, Click, G> renderer();
 
+  /**
+   * @return The normal items count that can be rendered. This value is equivalent to
+   * subtract the static {@link ItemContainer#nonNullCount()} to the {@link GUIDesignType#capacity()}.
+   */
   default int itemsPerPage() {
     return type().capacity() - staticItems().nonNullCount();
   }
 
+  /**
+   * Iterates through a row.
+   *
+   * @param row The row
+   * @param consumer The slot consumer
+   * @return This gui.
+   */
   @Contract("_, _ -> this")
   default @NotNull G iterateRow(int row, @NotNull BiConsumer<G, Integer> consumer) {
     if (!(type() instanceof GUIDesignType.Squared)) {
@@ -66,6 +123,13 @@ public interface GUI<Viewer, Item, Click, G extends GUI, Handle> extends Viewabl
     return gui;
   }
 
+  /**
+   * Iterates through a column.
+   *
+   * @param column The row
+   * @param consumer The slot consumer
+   * @return This gui.
+   */
   @Contract("_, _ -> this")
   default @NotNull G iterateColumn(int column, @NotNull BiConsumer<G, Integer> consumer) {
     if (!(type() instanceof GUIDesignType.Squared)) {
@@ -81,6 +145,12 @@ public interface GUI<Viewer, Item, Click, G extends GUI, Handle> extends Viewabl
     return gui;
   }
 
+  /**
+   * Iterates through the borders of this gui.
+   *
+   * @param consumer The slot consumer
+   * @return This gui.
+   */
   @Contract("_ -> this")
   default @NotNull G iterateBorders(@NotNull BiConsumer<G, Integer> consumer) {
     if (!(type() instanceof GUIDesignType.Squared)) {
